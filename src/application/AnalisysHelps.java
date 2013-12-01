a9bfcdadf5940337d53841f3b49ca52622536c7f
Артем <@172.17.225.164>
2013-12-01 18:49:08 +0200

    коммит в trunk

--- src/application/AnalisysHelps.java
+++ src/application/AnalisysHelps.java
@@ -13,5 +13,6 @@ public class AnalisysHelps {
     public static final String ADDITIONAL_ACTION_FOR_ONE = "Выберите дополнительное действие и/или часть тела и игрока";
     public static final String ADDITIONAL_ACTION_FOR_TWO_1 = "Выберите первого игрока";
     public static final String ADDITIONAL_ACTION_FOR_TWO_2 = "Выберите второго игрока";
+    public static final String ADDITIONAL_ACTION_WITH_BUTTON = "Выберите игрока(игроков) или нажмите Продолжить";
     public static final String PLACE = "Отметьте место ТТД на поле";
 }
